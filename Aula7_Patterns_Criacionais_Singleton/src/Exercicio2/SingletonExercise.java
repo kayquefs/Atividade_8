f69e975239ee516ffa2b5000f7f6fdb33959d143
Alunos <Alunos@MOCLI10122.animaedu.intranet>
2018-05-02 21:38:16 -0300

    Atividade8

---
+++ Aula7_Patterns_Criacionais_Singleton/src/Exercicio2/SingletonExercise.java
@@ -0,0 +1,12 @@
+package Exercicio2;
+
+//* Kayque Fernandes dos Santos 816121871
+
+public class SingletonExercise {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
